0629ca6494617b19c7e22418b4c10de3a0f782ae
NotKARAN0JR <150114776+NotKARAN0JR@users.noreply.github.com>
2025-03-13 19:43:15 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Earth\n";
+  cout << "Hello,  Earth\n";
   cout << "Hello, Jenkins\n";
   cout << "I have built this   \n";
   cout << "PES1UG22CS274   \n";
